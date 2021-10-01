f26d5a61ea4d679be2af7c2690158f20610f3fe6
AppCrashExpress <efimov.s2000@gmail.com>
2021-10-01 12:56:08 +0300

    Switch tabs to spaces

--- lab2/contours.cu
+++ lab2/contours.cu
@@ -2,14 +2,14 @@
 #include <fstream>
 #include <memory>
 
-#define CSC(call)  													\
-do {																\
-	cudaError_t res = call;											\
-	if (res != cudaSuccess) {										\
-		fprintf(stderr, "ERROR in %s:%d. Message: %s\n",			\
-				__FILE__, __LINE__, cudaGetErrorString(res));		\
-		exit(0);													\
-	}																\
+#define CSC(call)                                             \
+do {                                                          \
+    cudaError_t res = call;                                   \
+    if (res != cudaSuccess) {                                 \
+        fprintf(stderr, "ERROR in %s:%d. Message: %s\n",      \
+                __FILE__, __LINE__, cudaGetErrorString(res)); \
+        exit(0);                                              \
+    }                                                         \
 } while(0)
 
 texture<uchar4, 2, cudaReadModeElementType> tex;
@@ -46,7 +46,7 @@ void write_file(const std::unique_ptr<uchar4[]>& image,
 __device__
 double convert_greyscale(uchar4 pixel) {
     // Alpha ignored
-	return 0.299 * pixel.x + 
+    return 0.299 * pixel.x + 
            0.587 * pixel.y + 
            0.114 * pixel.z;
 }
@@ -54,9 +54,9 @@ double convert_greyscale(uchar4 pixel) {
 __global__
 void kernel(uchar4 *image, int32_t w, int32_t h) {
     int idx     = blockDim.x * blockIdx.x + threadIdx.x;
-	int idy     = blockDim.y * blockIdx.y + threadIdx.y;
-	int offsetx = blockDim.x * gridDim.x;
-	int offsety = blockDim.y * gridDim.y;
+    int idy     = blockDim.y * blockIdx.y + threadIdx.y;
+    int offsetx = blockDim.x * gridDim.x;
+    int offsety = blockDim.y * gridDim.y;
 
     int x_filter[3][3] = {
         {-1,  0,  1},
@@ -69,8 +69,8 @@ void kernel(uchar4 *image, int32_t w, int32_t h) {
         { 1,  1,  1}
     };
 
-	for(int y = idy; y < h; y += offsety) {
-		for(int x = idx; x < w; x += offsetx) {
+    for(int y = idy; y < h; y += offsety) {
+        for(int x = idx; x < w; x += offsetx) {
             double gx = 0;
             double gy = 0;
 
@@ -90,7 +90,7 @@ void kernel(uchar4 *image, int32_t w, int32_t h) {
             image[offset].y = g;
             image[offset].z = g;
             image[offset].w = 0;
-		}
+        }
     }
 
 }
@@ -108,26 +108,26 @@ int main() {
     cudaArray *arr;
     cudaChannelFormatDesc ch = cudaCreateChannelDesc<uchar4>();
     CSC(cudaMallocArray(&arr, &ch, w, h));
-	CSC(cudaMemcpyToArray(arr, 0, 0, image.get(), sizeof(uchar4) * w * h, cudaMemcpyHostToDevice));
+    CSC(cudaMemcpyToArray(arr, 0, 0, image.get(), sizeof(uchar4) * w * h, cudaMemcpyHostToDevice));
 
     tex.addressMode[0] = cudaAddressModeClamp;
-	tex.addressMode[1] = cudaAddressModeClamp;
-	tex.channelDesc = ch;
-	tex.filterMode = cudaFilterModePoint;
-	tex.normalized = false;
-	CSC(cudaBindTextureToArray(tex, arr, ch));
+    tex.addressMode[1] = cudaAddressModeClamp;
+    tex.channelDesc = ch;
+    tex.filterMode = cudaFilterModePoint;
+    tex.normalized = false;
+    CSC(cudaBindTextureToArray(tex, arr, ch));
 
-	uchar4 *dev_image;
-	CSC(cudaMalloc(&dev_image, sizeof(uchar4) * w * h));
+    uchar4 *dev_image;
+    CSC(cudaMalloc(&dev_image, sizeof(uchar4) * w * h));
 
     kernel<<< dim3(32, 32), dim3(32, 32) >>>(dev_image, w, h);
 
-	CSC(cudaMemcpy(image.get(), dev_image, sizeof(uchar4) * w * h, cudaMemcpyDeviceToHost));
+    CSC(cudaMemcpy(image.get(), dev_image, sizeof(uchar4) * w * h, cudaMemcpyDeviceToHost));
 
-	CSC(cudaUnbindTexture(tex));
+    CSC(cudaUnbindTexture(tex));
 
-	CSC(cudaFreeArray(arr));
-	CSC(cudaFree(dev_image));
+    CSC(cudaFreeArray(arr));
+    CSC(cudaFree(dev_image));
 
     write_file(image, w, h, out_file);
 }
