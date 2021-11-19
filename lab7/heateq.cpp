062612494478531e5d8735a565ff18bd26390ebb
AppCrashExpress <efimov.s2000@gmail.com>
2021-11-19 14:30:06 +0300

    Add heat calculation

--- lab7/heateq.cpp
+++ lab7/heateq.cpp
@@ -62,24 +62,30 @@ int main(int argc, char **argv) {
     int proc_y = (id / proc_size[x_dir]) % proc_size[y_dir];
     int proc_z =  id / (proc_size[x_dir] * proc_size[y_dir]);
 
-    // Pad out elements for boundary values
-    // Remember that it is padded out
-    // REMEMBER THAT IT IS PADDED OUT
+    int pad_block_size[3];
     
-    block_size[x_dir] += 2;
-    block_size[y_dir] += 2;
-    block_size[z_dir] += 2;
+    pad_block_size[x_dir] = block_size[x_dir] + 2;
+    pad_block_size[y_dir] = block_size[y_dir] + 2;
+    pad_block_size[z_dir] = block_size[z_dir] + 2;
 
-    size_t elem_count = block_size[x_dir] * block_size[y_dir] * block_size[z_dir];
+    size_t elem_count = pad_block_size[x_dir] * pad_block_size[y_dir] * pad_block_size[z_dir];
+    double *old_grid = new double[elem_count];
+    double *new_grid = new double[elem_count];
+    
+    int edge_side_len = std::max({ block_size[x_dir], block_size[y_dir], block_size[z_dir] });
+    int edge_buff_size = edge_side_len * edge_side_len;
+    double *edge_buff = new double[edge_buff_size];
 
-    int max_block_size = std::max({ block_size[x_dir], block_size[y_dir], block_size[z_dir] });
+    auto calc_1d = [pad_block_size](int x, int y, int z) {
+        return (z * pad_block_size[y_dir] + y) * pad_block_size[x_dir] + x;
+    };
 
-    double *prev_grid = new double[elem_count];
-    double *new_grid  = new double[elem_count];
-    double *edge_buff = new double[max_block_size];
+    auto calc_edge = [edge_side_len](int x, int y) {
+        return y * edge_side_len + x;
+    };
 
-    auto calc_1d = [block_size](int x, int y, int z) {
-        return (z * block_size[y_dir] + y) * block_size[x_dir] + x;
+    auto calc_rank = [proc_size](int x, int y, int z) {
+        return (z * proc_size[y_dir] + y) * proc_size[x_dir] + x;
     };
 
     // --------------- //
@@ -87,63 +93,286 @@ int main(int argc, char **argv) {
     // --------------- //
     
     for (size_t i = 0; i < elem_count; ++i) {
-        prev_grid[i] = u0;
+        old_grid[i] = u0;
     }
 
-    // Edge elements aren't used, 
+    // Fill border elements once for both arrays
+    // Edge and corner elements aren't used, 
     // do whatever with them
 
     if (proc_x == 0) {
-        for (int j = 0; j < block_size[z_dir]; ++j) {
-            for (int i = 0; i < block_size[y_dir]; ++i) {
-                prev_grid[calc_1d(0, i, j)] = u[left_s];
+        for (int j = 0; j < pad_block_size[z_dir]; ++j) {
+            for (int i = 0; i < pad_block_size[y_dir]; ++i) {
+                old_grid[calc_1d(0, i, j)] = u[left_s];
+                new_grid[calc_1d(0, i, j)] = u[left_s];
             }
         }
     }
 
     if (proc_x == proc_size[x_dir] - 1) {
-        for (int j = 0; j < block_size[z_dir]; ++j) {
-            for (int i = 0; i < block_size[y_dir]; ++i) {
-                prev_grid[calc_1d(block_size[x_dir] - 1, i, j)] = u[right_s];
+        for (int j = 0; j < pad_block_size[z_dir]; ++j) {
+            for (int i = 0; i < pad_block_size[y_dir]; ++i) {
+                old_grid[calc_1d(pad_block_size[x_dir] - 1, i, j)] = u[right_s];
+                new_grid[calc_1d(pad_block_size[x_dir] - 1, i, j)] = u[right_s];
             }
         }
     }
 
     if (proc_y == 0) {
-        for (int j = 0; j < block_size[z_dir]; ++j) {
-            for (int i = 0; i < block_size[x_dir]; ++i) {
-                prev_grid[calc_1d(i, 0, j)] = u[up_s];
+        for (int j = 0; j < pad_block_size[z_dir]; ++j) {
+            for (int i = 0; i < pad_block_size[x_dir]; ++i) {
+                old_grid[calc_1d(i, 0, j)] = u[front_s];
+                new_grid[calc_1d(i, 0, j)] = u[front_s];
             }
         }
     }
 
     if (proc_y == proc_size[y_dir] - 1) {
-        for (int j = 0; j < block_size[z_dir]; ++j) {
-            for (int i = 0; i < block_size[x_dir]; ++i) {
-                prev_grid[calc_1d(i, block_size[y_dir] - 1, j)] = u[down_s];
+        for (int j = 0; j < pad_block_size[z_dir]; ++j) {
+            for (int i = 0; i < pad_block_size[x_dir]; ++i) {
+                old_grid[calc_1d(i, pad_block_size[y_dir] - 1, j)] = u[back_s];
+                new_grid[calc_1d(i, pad_block_size[y_dir] - 1, j)] = u[back_s];
             }
         }
     }
 
     if (proc_z == 0) {
         // 
-        for (int j = 0; j < block_size[y_dir]; ++j) {
-            for (int i = 0; i < block_size[x_dir]; ++i) {
-                prev_grid[calc_1d(i, j, 0)] = u[front_s];
+        for (int j = 0; j < pad_block_size[y_dir]; ++j) {
+            for (int i = 0; i < pad_block_size[x_dir]; ++i) {
+                old_grid[calc_1d(i, j, 0)] = u[down_s];
+                new_grid[calc_1d(i, j, 0)] = u[down_s];
             }
         }
     }
 
     if (proc_z == proc_size[z_dir] - 1) {
         // 
-        for (int j = 0; j < block_size[y_dir]; ++j) {
-            for (int i = 0; i < block_size[x_dir]; ++i) {
-                prev_grid[calc_1d(i, j, block_size[z_dir] - 1)] = u[back_s];
+        for (int j = 0; j < pad_block_size[y_dir]; ++j) {
+            for (int i = 0; i < pad_block_size[x_dir]; ++i) {
+                old_grid[calc_1d(i, j, pad_block_size[z_dir] - 1)] = u[up_s];
+                new_grid[calc_1d(i, j, pad_block_size[z_dir] - 1)] = u[up_s];
             }
         }
     }
 
-    delete[] prev_grid;
+    // -------------- //
+    // Calculate grid //
+    // -------------- //
+
+    int proc_count = proc_size[x_dir] * proc_size[y_dir] * proc_size[z_dir];
+    double *block_maxes = new double[proc_count];
+
+    int buffer_size;
+    MPI_Pack_size(edge_buff_size, MPI_DOUBLE, MPI_COMM_WORLD, &buffer_size);
+    buffer_size = 6 * (buffer_size + MPI_BSEND_OVERHEAD);
+    double *send_buffer = new double[buffer_size];
+    MPI_Buffer_attach(send_buffer, buffer_size);
+
+    MPI_Status status;
+
+    double hx = 1.0f * l[x_dir] / (proc_size[x_dir] * pad_block_size[x_dir]);
+    double hy = 1.0f * l[y_dir] / (proc_size[y_dir] * pad_block_size[y_dir]);
+    double hz = 1.0f * l[z_dir] / (proc_size[z_dir] * pad_block_size[z_dir]);
+    double inv_hx2 = 1.0f / (hx * hx);
+    double inv_hy2 = 1.0f / (hy * hy);
+    double inv_hz2 = 1.0f / (hz * hz);
+
+    while (true) {
+        double max_diff = 0;
+        for (int z = 1; z < pad_block_size[z_dir] - 1; ++z) {
+            for (int y = 1; y < pad_block_size[y_dir] - 1; ++y) {
+                for (int x = 1; x < pad_block_size[x_dir] - 1; ++x) {
+                    double val = (
+                        (old_grid[calc_1d(x+1, y,   z  )] + old_grid[calc_1d(x-1, y,   z  )]) * inv_hx2 +
+                        (old_grid[calc_1d(x,   y+1, z  )] + old_grid[calc_1d(x,   y-1, z  )]) * inv_hy2 +
+                        (old_grid[calc_1d(x,   y,   z+1)] + old_grid[calc_1d(x,   y,   z-1)]) * inv_hz2
+                    ) / (2 * (inv_hx2 + inv_hy2 + inv_hz2));
+                    new_grid[calc_1d(x, y, z)] = val;
+                    max_diff = std::max( max_diff, std::abs(val - old_grid[calc_1d(x, y, z)]) );
+                }
+            }
+        }
+
+        MPI_Allgather(&max_diff, 1, MPI_DOUBLE, block_maxes, 1, MPI_DOUBLE, MPI_COMM_WORLD);
+        for (int i = 0; i < proc_count; ++i) {
+            max_diff = std::max(max_diff, block_maxes[i]);
+        }
+
+        if (max_diff < eps) {
+            std::swap(old_grid, new_grid);
+            break;
+        }
+
+        // Send buffers to neighbours
+        // Visually copies like:
+        //
+        // n-2 n-1  |   0   1
+        //  |   ↑       ↑   |
+        //  |   |-------|---|
+        //  |-----------|
+
+        if (proc_x < proc_size[x_dir] - 1) {
+            for (int z = 1; z < pad_block_size[z_dir] - 1; ++z) {
+                for (int y = 1; y < pad_block_size[y_dir] - 1; ++y) {
+                    edge_buff[calc_edge(y-1, z-1)] = new_grid[calc_1d(pad_block_size[x_dir] - 2, y, z)];
+                }
+            }
+            MPI_Bsend(edge_buff,
+                      edge_buff_size,
+                      MPI_DOUBLE,
+                      calc_rank(proc_x + 1, proc_y, proc_z),
+                      id,
+                      MPI_COMM_WORLD);
+        }
+
+        if (proc_x > 0) {
+            for (int z = 1; z < pad_block_size[z_dir] - 1; ++z) {
+                for (int y = 1; y < pad_block_size[y_dir] - 1; ++y) {
+                    edge_buff[calc_edge(y-1, z-1)] = new_grid[calc_1d(1, y, z)];
+                }
+            }
+            MPI_Bsend(edge_buff,
+                      edge_buff_size,
+                      MPI_DOUBLE,
+                      calc_rank(proc_x - 1, proc_y, proc_z),
+                      id,
+                      MPI_COMM_WORLD);
+        }
+
+        if (proc_y < proc_size[y_dir] - 1) {
+            for (int z = 1; z < pad_block_size[z_dir] - 1; ++z) {
+                for (int x = 1; x < pad_block_size[x_dir] - 1; ++x) {
+                    edge_buff[calc_edge(x-1, z-1)] = new_grid[calc_1d(x, pad_block_size[y_dir] - 2, z)];
+                }
+            }
+            MPI_Bsend(edge_buff,
+                      edge_buff_size,
+                      MPI_DOUBLE,
+                      calc_rank(proc_x, proc_y + 1, proc_z),
+                      id,
+                      MPI_COMM_WORLD);
+        }
+
+        if (proc_y > 0) {
+            for (int z = 1; z < pad_block_size[z_dir] - 1; ++z) {
+                for (int x = 1; x < pad_block_size[x_dir] - 1; ++x) {
+                    edge_buff[calc_edge(x-1, z-1)] = new_grid[calc_1d(x, 1, z)];
+                }
+            }
+            MPI_Bsend(edge_buff,
+                      edge_buff_size,
+                      MPI_DOUBLE,
+                      calc_rank(proc_x, proc_y - 1, proc_z),
+                      id,
+                      MPI_COMM_WORLD);
+        }
+
+        if (proc_z < proc_size[z_dir] - 1) {
+            for (int y = 1; y < pad_block_size[y_dir] - 1; ++y) {
+                for (int x = 1; x < pad_block_size[x_dir] - 1; ++x) {
+                    edge_buff[calc_edge(x-1, y-1)] = new_grid[calc_1d(x, y, pad_block_size[z_dir] - 2)];
+                }
+            }
+            MPI_Bsend(edge_buff,
+                      edge_buff_size,
+                      MPI_DOUBLE,
+                      calc_rank(proc_x, proc_y, proc_z + 1),
+                      id,
+                      MPI_COMM_WORLD);
+        }
+
+        if (proc_z > 0) {
+            for (int y = 1; y < pad_block_size[y_dir] - 1; ++y) {
+                for (int x = 1; x < pad_block_size[x_dir] - 1; ++x) {
+                    edge_buff[calc_edge(x-1, y-1)] = new_grid[calc_1d(x, y, 1)];
+                }
+            }
+            MPI_Bsend(edge_buff,
+                      edge_buff_size,
+                      MPI_DOUBLE,
+                      calc_rank(proc_x, proc_y, proc_z - 1),
+                      id,
+                      MPI_COMM_WORLD);
+        }
+
+        // Recieve buffers from neighbours
+
+        if (proc_x < proc_size[x_dir] - 1) {
+            MPI_Recv(edge_buff, edge_buff_size, MPI_DOUBLE, calc_rank(proc_x + 1, proc_y, proc_z), calc_rank(proc_x + 1, proc_y, proc_z), MPI_COMM_WORLD, &status);
+            for (int z = 1; z < pad_block_size[z_dir] - 1; ++z) {
+                for (int y = 1; y < pad_block_size[y_dir] - 1; ++y) {
+                    new_grid[calc_1d(pad_block_size[x_dir] - 1, y, z)] = edge_buff[calc_edge(y-1, z-1)];
+                }
+            }
+        }
+
+        if (proc_x > 0) {
+            MPI_Recv(edge_buff, edge_buff_size, MPI_DOUBLE, calc_rank(proc_x - 1, proc_y, proc_z), calc_rank(proc_x - 1, proc_y, proc_z), MPI_COMM_WORLD, &status);
+            for (int z = 1; z < pad_block_size[z_dir] - 1; ++z) {
+                for (int y = 1; y < pad_block_size[y_dir] - 1; ++y) {
+                    new_grid[calc_1d(0, y, z)] = edge_buff[calc_edge(y-1, z-1)];
+                }
+            }
+        }
+
+        if (proc_y < proc_size[y_dir] - 1) {
+            MPI_Recv(edge_buff, edge_buff_size, MPI_DOUBLE, calc_rank(proc_x, proc_y + 1, proc_z), calc_rank(proc_x, proc_y + 1, proc_z), MPI_COMM_WORLD, &status);
+            for (int z = 1; z < pad_block_size[z_dir] - 1; ++z) {
+                for (int x = 1; x < pad_block_size[x_dir] - 1; ++x) {
+                    new_grid[calc_1d(x, pad_block_size[y_dir] - 1, z)] = edge_buff[calc_edge(x-1, z-1)];
+                }
+            }
+        }
+
+        if (proc_y > 0) {
+            MPI_Recv(edge_buff, edge_buff_size, MPI_DOUBLE, calc_rank(proc_x, proc_y - 1, proc_z), calc_rank(proc_x, proc_y - 1, proc_z), MPI_COMM_WORLD, &status);
+            for (int z = 1; z < pad_block_size[z_dir] - 1; ++z) {
+                for (int x = 1; x < pad_block_size[x_dir] - 1; ++x) {
+                    new_grid[calc_1d(x, 0, z)] = edge_buff[calc_edge(x-1, z-1)];
+                }
+            }
+        }
+
+        if (proc_z < proc_size[z_dir] - 1) {
+            MPI_Recv(edge_buff, edge_buff_size, MPI_DOUBLE, calc_rank(proc_x, proc_y, proc_z + 1), calc_rank(proc_x, proc_y, proc_z + 1), MPI_COMM_WORLD, &status);
+            for (int y = 1; y < pad_block_size[y_dir] - 1; ++y) {
+                for (int x = 1; x < pad_block_size[x_dir] - 1; ++x) {
+                    new_grid[calc_1d(x, y, pad_block_size[z_dir] - 1)] = edge_buff[calc_edge(x-1, y-1)];
+                }
+            }
+        }
+
+        if (proc_z > 0) {
+            MPI_Recv(edge_buff, edge_buff_size, MPI_DOUBLE, calc_rank(proc_x, proc_y, proc_z - 1), calc_rank(proc_x, proc_y, proc_z - 1), MPI_COMM_WORLD, &status);
+            for (int y = 1; y < pad_block_size[y_dir] - 1; ++y) {
+                for (int x = 1; x < pad_block_size[x_dir] - 1; ++x) {
+                    new_grid[calc_1d(x, y, 0)] = edge_buff[calc_edge(x-1, y-1)];
+                }
+            }
+        }
+
+        std::swap(old_grid, new_grid);
+    }
+
+
+    std::cout << id << '\n';
+    for (int z = 0; z < pad_block_size[z_dir]; ++z) {
+        for (int y = 0; y < pad_block_size[y_dir]; ++y) {
+            for (int x = 0; x < pad_block_size[x_dir]; ++x) {
+                std::cout << old_grid[calc_1d(x, y, z)] << ' ';
+            }
+            std::cout << '\n';
+        }
+        std::cout << '\n';
+    }
+    std::cout << std::flush;
+
+    MPI_Buffer_detach(&send_buffer, &buffer_size);
+
+    delete[] block_maxes;
+    delete[] send_buffer;
+    delete[] old_grid;
     delete[] new_grid;
     delete[] edge_buff;
 
