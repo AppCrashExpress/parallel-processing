d61b9451abd7a7dab8eee4f6139cffb926c701fe
student31415 <qwerty65k@mail.ru>
2021-12-25 00:12:38 +0300

    pre parallel work

--- cp/src/main.cpp
+++ cp/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <memory>
 
 #include "tgaimage.h"
 #include "linear.h"
@@ -12,14 +13,21 @@
 
 using IntersectType = std::pair<Vector3D, int>;
 
-void build_space(std::vector<Polygon>& polys,
-                 std::vector<Light>& lights,
-                 std::vector<Texture>& textures) {
+
+std::vector<Polygon> polygons;
+std::vector<Light> lights;
+std::vector<Texture> textures;
+
+size_t polygons_size,  lights_size, textures_size;
+
+
+
+void build_space() {
     Pixel pixels[4] = {
-        {255,   0,   0, 0.0f, 0.7f,},
-        {  0, 255,   0, 0.0f, 0.7f,},
-        {  0,   0, 255, 0.0f, 0.7f,},
-        {255, 255, 255, 0.0f, 0.7f,},
+        {255,   0,   0, 0.7f, 0.7f,},
+        {  0, 255,   0, 0.7f, 0.7f,},
+        {  0,   0, 255, 0.7f, 0.7f,},
+        {255, 255, 255, 0.7f, 0.7f,},
     };
     textures.push_back(Texture(2, 2, pixels));
 
@@ -39,46 +47,49 @@ void build_space(std::vector<Polygon>& polys,
     pixel.r = 0;
     pixel.b = 0;
     pixel.g = 255;
-    pixel.refract = 0.95;
+    pixel.reflect = 0.25;
     textures.push_back(Texture(1, 1, &pixel));
 
-    pixel.r = 255;
-    pixel.b = 255;
-    pixel.g = 50;
-    pixel.reflect = 0.25;
+    pixel.r = 100;
+    pixel.b = 100;
+    pixel.g = 100;
+    pixel.reflect = 1.0;
     pixel.refract = 0;
     textures.push_back(Texture(1, 1, &pixel));
 
     std::vector<Polygon> cube;
 
     cube = construct_cube(Vector3D(0, 0, 0), 1.0f, 0);
-    polys.insert(polys.end(), cube.begin(), cube.end());
+    polygons.insert(polygons.end(), cube.begin(), cube.end());
 
     cube = construct_cube(Vector3D(5.0f, 0, 0), 1.0f, 1);
-    polys.insert(polys.end(), cube.begin(), cube.end());
+    polygons.insert(polygons.end(), cube.begin(), cube.end());
 
     cube = construct_cube(Vector3D(0, 5.0f, 0), 1.0f, 2);
-    polys.insert(polys.end(), cube.begin(), cube.end());
+    polygons.insert(polygons.end(), cube.begin(), cube.end());
 
     cube = construct_cube(Vector3D(0, 0, 5.0f), 1.0f, 3);
-    polys.insert(polys.end(), cube.begin(), cube.end());
+    polygons.insert(polygons.end(), cube.begin(), cube.end());
 
     std::vector<Polygon> floor = construct_floor(Vector3D(0.0f, -2.0f, 0.0f), 10, 4);
-    polys.insert(polys.end(), floor.begin(), floor.end());
+    polygons.insert(polygons.end(), floor.begin(), floor.end());
 
     const Vector3D light_color = Vector3D(1.0f, 1.0f, 1.0f);
 
     lights = std::vector<Light>{{Vector3D(2.0f, 0.0f, 0.0f), light_color},
                                 {Vector3D(0.0f, 2.0f, 0.0f), light_color}};
+    
+    polygons_size = polygons.size();  
+    lights_size   = lights.size(); 
+    textures_size = textures.size();                 
 }
 
-std::pair<Vector3D, int> intersect_polygons(const std::vector<Polygon>& polygons,
-                            const Ray& ray) {
+std::pair<Vector3D, int> intersect_polygons(const Ray& ray) {
     Vector3D max_res;
     int max_poly = -1;
 
     max_res[0] = 1e10;
-    for (int i = 0; i < polygons.size(); ++i) {
+    for (int i = 0; i < polygons_size; ++i) {
         std::pair<Vector3D, bool> res = polygons[i].find_intersection(ray, 1e-7);
         if (res.second && max_res[0] > res.first[0]) {
             max_res = res.first;
@@ -89,9 +100,7 @@ std::pair<Vector3D, int> intersect_polygons(const std::vector<Polygon>& polygons
     return {max_res, max_poly};
 }
 
-Pixel retrieve_intersection_pixel(const std::vector<Polygon>& polygons,
-                                  const std::vector<Texture>& textures,
-                                  const IntersectType& intersection) {
+Pixel retrieve_intersection_pixel(const IntersectType& intersection) {
 
     const float t = intersection.first[0];
     const float u = intersection.first[1];
@@ -107,13 +116,10 @@ Pixel retrieve_intersection_pixel(const std::vector<Polygon>& polygons,
 }
 
 std::vector<char> 
-check_visible_lights(const std::vector<Polygon>& polygons,
-        const std::vector<Texture>& textures,
-        const std::vector<Light>& lights,
-        const Vector3D& pos) {
-    std::vector<char> visible(lights.size(), false);
+check_visible_lights(const Vector3D& pos) {
+    std::vector<char> visible(lights_size, false);
 
-    for (size_t i = 0; i < lights.size(); ++i) {
+    for (size_t i = 0; i < lights_size; ++i) {
         Vector3D dir = pos - lights[i].pos;
         float t_max = dir.get_length() - 1e-4f;
         dir = dir.get_norm();
@@ -121,11 +127,11 @@ check_visible_lights(const std::vector<Polygon>& polygons,
         Ray ray;
         ray.pos = lights[i].pos;
         ray.dir = dir;
-        IntersectType intersect = intersect_polygons(polygons, ray);
+        IntersectType intersect = intersect_polygons(ray);
 
         float t = intersect.first[0];
         if (t < 0 || t >= t_max) {
-            Pixel hit_pix = retrieve_intersection_pixel(polygons, textures, intersect);
+        //    Pixel hit_pix = retrieve_intersection_pixel(intersect);
             visible[i] = true;
         }
     }
@@ -135,13 +141,10 @@ check_visible_lights(const std::vector<Polygon>& polygons,
 
 Vector3D shade_pixel(const Pixel& pix,
                      const Vector3D& norm,
-                     const Vector3D& intersect_pos,
-                     const std::vector<Polygon>& polygons,
-                     const std::vector<Texture>& textures,
-                     const std::vector<Light>& lights) {
+                     const Vector3D& intersect_pos) {
     Vector3D colors;
 
-    std::vector<char> visible = check_visible_lights(polygons, textures, lights, intersect_pos);
+    std::vector<char> visible = check_visible_lights(intersect_pos);
     for (int l = 0; l < visible.size(); ++l) {
         if (!visible[l]) {
             continue;
@@ -163,11 +166,9 @@ Vector3D shade_pixel(const Pixel& pix,
 }
 
 
-Vector3D cast_ray(const std::vector<Polygon>& polygons,
-                  const std::vector<Light>& lights,
-                  const std::vector<Texture>& textures,
-                  const Ray& ray) {
-    IntersectType intersect = intersect_polygons(polygons, ray);
+Vector3D cast_ray(Ray& ray, Ray& reflection_ray) {
+
+    IntersectType intersect = intersect_polygons(ray);
     const float t = intersect.first[0];
     const int polygon_id = intersect.second;
     Vector3D color;
@@ -175,69 +176,128 @@ Vector3D cast_ray(const std::vector<Polygon>& polygons,
     if (polygon_id != -1) {
         Vector3D intersect_pos = ray.pos + t * ray.dir;
 
-        Pixel pix = retrieve_intersection_pixel(polygons, textures, intersect);
-        
-        color += shade_pixel(pix, polygons[polygon_id].norm(),
-                             intersect_pos, polygons, textures, lights);
+        Pixel pix = retrieve_intersection_pixel(intersect);
+        color += shade_pixel(pix, polygons[polygon_id].norm(), intersect_pos);
 
-        color = color * (1 - pix.refract);
+        ray.color = color;
 
         // Use small value just in case for now
-        if (pix.reflect > 0.05f) {
+        if (pix.reflect > 0.03f) {
             const Vector3D& norm = polygons[polygon_id].norm();
             const Vector3D reflect_vec = Vector3D::reflect(ray.dir, norm);
-            Ray reflect_ray;
-            reflect_ray.pos = intersect_pos + 0.05 * norm;
-            reflect_ray.dir = reflect_vec;
-            color += pix.reflect * cast_ray(
-                    polygons, lights, textures, reflect_ray);
+            
+            reflection_ray.pos = intersect_pos + 0.03 * norm;
+            reflection_ray.dir = reflect_vec;
+
+            reflection_ray.consid_coef =  pix.reflect * ray.consid_coef;
+            reflection_ray.x = ray.x;
+            reflection_ray.y = ray.y;
+
         }
-        if (pix.refract > 0.05f) {
-            // Move forward a bit, past polygon
-            Ray refract_ray;
-            refract_ray.dir = ray.dir;
-            refract_ray.pos = ray.pos + 0.15 * ray.dir;
-            color += pix.refract * cast_ray(
-                    polygons, lights, textures, refract_ray);
-        }
+
+    }else{
+        //reflection_ray.use = false;
+        ray.color = {0.0f, 0.0f, 0.0f};
+        
     }
 
     return color;
 }
 
-void render(const std::vector<Polygon>& polygons,
-            const std::vector<Light>& lights,
-            const std::vector<Texture>& textures,
-            const Vector3D& pc,
+
+void
+do_rays(Ray* cur_rays, Ray* next_rays, size_t cur_size){
+    for(size_t i = 0 ; i < cur_size; ++i) {
+        cast_ray(cur_rays[i], next_rays[i]);
+    }
+}
+
+
+void
+do_colors(Vector3D* image_buff, Ray* rays,const int rays_size, const int h){
+
+    for (int i = 0; i < rays_size; ++i){
+        int x = rays[i].x, 
+            y = rays[i].y;
+
+        image_buff[y*h+x] += rays[i].color*rays[i].consid_coef;
+    }
+}
+
+int
+compact(Ray* rays, int rays_size){
+    int cnt=0; 
+    for(int i = 0; i < rays_size; ++i)
+        if(rays[i].use){
+            rays[cnt++]=rays[i];
+        }
+    return cnt;
+    
+}
+
+void 
+set_image(Vector3D* image_buff, TGAImage& image, int w, int h ){
+    for(int i = 0 ; i < w ; ++i)
+        for(int j = 0 ; j < h ; ++j){
+            const Vector3D& color_vec = image_buff[i*h+j];
+            TGAColor color(
+                std::min(color_vec.x(), 255.0f),
+                std::min(color_vec.y(), 255.0f),
+                std::min(color_vec.z(), 255.0f)
+            );
+            image.set(i, j, color);
+        }
+
+
+}
+
+void render(const Vector3D& pc,
             const Vector3D& pv,
             int w, int h,
             float angle,
-            TGAImage& image) {
+            TGAImage& image, 
+            const int max_depth) {
     float dw = 2.0f / (w - 1);
     float dh = 2.0f / (h - 1);
-    float z = 1.0f / tan(angle * M_PI / 360.0f);
+    float z  = 1.0f / tan(angle * M_PI / 360.0f);
     
     Matrix4x4 look_matr;
     look_matr.look_at(pv, pc, Vector3D(0.0f, 1.0f, 0.0f));
+
+
+    size_t pixel_cnt = w*h;
+    
+    std::unique_ptr<Vector3D[]> image_buff (new Vector3D[pixel_cnt]);
+    std::unique_ptr<Ray[]>      cur_rays   (new Ray[pixel_cnt]);
+    std::unique_ptr<Ray[]>      next_rays  (new Ray[pixel_cnt]);
+
 
     for (int i = 0; i < w; ++i) {
         for (int j = 0; j < h; ++j) {
             Vector3D v = Vector3D(-1.0f + dw * i, (-1.0f + dh * j) * h / w, z);
             Vector3D dir = (v * look_matr).get_norm();
-            Ray ray;
-            ray.pos = pc;
-            ray.dir = dir;
+            cur_rays[i*h+j] = Ray(pc, dir, j, i);
 
-            Vector3D color_vec = cast_ray(polygons, lights, textures, ray);
-
-            TGAColor color(
-                    std::min(color_vec.x(), 255.0f),
-                    std::min(color_vec.y(), 255.0f),
-                    std::min(color_vec.z(), 255.0f)
-                );
-            image.set(i, j, color);
         }
     }
+
+    int cur_size = pixel_cnt;
+
+    for (int depth = 0; depth < max_depth; ++depth) {
+    
+        do_rays (cur_rays.get(), next_rays.get(), cur_size);
+
+
+        do_colors(image_buff.get(),cur_rays.get(), cur_size, h);
+        
+        cur_size = compact(next_rays.get(), cur_size);
+        
+        std::swap(cur_rays, next_rays);
+
+        set_image(image_buff.get(), image, w,h);
+
+    }
+    
 }
 
 int main() {
@@ -246,15 +306,14 @@ int main() {
     char buff[256];
     TGAImage image(w, h, TGAImage::Format::RGBA);
 
-    std::vector<Polygon> polygons;
-    std::vector<Texture> textures;
-    std::vector<Light> lights;
-    build_space(polygons, lights, textures);
+    build_space();
 
-    for (int k = 0; k < 1; ++k) {
-        Vector3D pc = Vector3D(6.0f, 6.0f, 6.0f);
+    int max_depth = 3;
+    for (int k = 0; k < 10; ++k) {
+        max_depth++;
+        Vector3D pc = Vector3D( 2.0f +3.0f*sin(k*0.2f),  3.0f + 3.0f*cos(k*0.1f), 3.0f + 3.0f*sin(k*0.1f));
         Vector3D pv = Vector3D(0.0f, 0.0f, 0.0f);
-        render(polygons, lights, textures, pc, pv, w, h, 120.0, image);
+        render(pc, pv, w, h, 120.0, image, max_depth);
 
         sprintf(buff, "res/%d.tga", k);
         image.write_tga_file(buff, true, false);
--- cp/src/ray.h
+++ cp/src/ray.h
@@ -5,8 +5,20 @@
 
 class Ray {
 public:
+    Ray(): use(true), consid_coef(1.0){}
+    Ray(const Vector3D& p, const Vector3D& d, int _x, int _y): pos(p), dir(d), x(_x), y(_y), use(true), consid_coef(1.0){}
+
+
+
     Vector3D pos;
     Vector3D dir;
+    Vector3D color;
+    bool use;
+    float consid_coef;
+    int x;
+    int y;
+
+
 private:
 
 };
